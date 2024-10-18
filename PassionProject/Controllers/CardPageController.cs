b20cce2720c0a1d24b1bc5f8ddd999e286fb0e54
Marcus Jeong <marcus.jeong96@gmail.com>
2024-10-18 12:55:29 -0400

    fixed a problem with the DB types and related keys to use the "colors" db with controllers

---
+++ PassionProject/Controllers/CardPageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassionProject.Interfaces;
+using PassionProject.Models;
+using PassionProject.Services;
+
+
+namespace PassionProject.Controllers
+{
+    public class CardPageController : Controller
+    {
+        private readonly ICardService _cardService;
+
+        // dependency injection of service intergace
+        public CardPageController(ICardService cardService)
+        {
+            _cardService = cardService;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        // GET: /cards/List
+        public async Task<IActionResult> List()
+        {
+            IEnumerable<Card> Cards = await _cardService.ListCards();
+            return View(Cards);
+        }
+    }
+}
