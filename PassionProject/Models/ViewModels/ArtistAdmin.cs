30e586405120ab46ebead5ff0fffa9bcec5a6afd
Marcus Jeong <marcus.jeong96@gmail.com>
2024-12-13 17:00:21 -0500

    added client side conditional rendering for CRUD elements. Added way to access Card-Album relational table on landing page

---
+++ PassionProject/Models/ViewModels/ArtistAdmin.cs
@@ -0,0 +1,11 @@
+﻿namespace PassionProject.Models.ViewModels
+{
+    public class ArtistAdmin
+    {
+        // This ViewModel is the structure needed for us to render
+        // ArtistsPage/List.cshtml
+
+        public IEnumerable<ArtistDto> Artists { get; set; }
+        public bool isAdmin { get; set; }
+    }
+}
